2fcaa2d2cab5ac010b7afcc7b8c23fd705103689
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-11-17 22:59:41 +0100

    Add TextMate grammar

--- TD5_extended/test_all.sh
+++ TD5_extended/test_all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 rm Resources/*.am
-for x in Resources/*.in
+for x in Resources/*.td5
 do
   echo $x
   export M=$(basename ${x%.*})
   (
-    ./cmake-build-debug/turing Resources/$M.in -O > Resources/$M.am \
+    ./cmake-build-debug/turing Resources/$M.td5 -O > Resources/$M.am \
     && cd ../../Automatic-Machines-Simulator/ \
     && python.exe -m am sim ../tdcompil/TD5_extended/Resources/$M.am -r
   )
